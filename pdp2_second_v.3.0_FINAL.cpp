14c2f7f7517d1fe3860e46fd6c03e1bbcb7c1deb
Peter Tsiompanis <38920641+Tsiompanis@users.noreply.github.com>
2020-02-18 22:07:16 +0200

    Update pdp2_second_v.3.0_FINAL.cpp

--- pdp2_second_v.3.0_FINAL.cpp
+++ pdp2_second_v.3.0_FINAL.cpp
@@ -15,17 +15,17 @@ int data[1000005];
 int main()
 {
 	//initializing freopen functions  
-    	freopen("longsumk.in","r",stdin);        
-    	freopen("longsumk.out","w",stdout);      
+    freopen("longsumk.in","r",stdin);        
+    freopen("longsumk.out","w",stdout);      
 	
 	//initializing variables and scanning them
-	int n, k, out, current, start;
+	int n, k, out, current_sum, start;
 	scanf("%d %d",&n, &k);
 	
 	//scanning the first day's value and put it at the 0th index so it doesn't get miscomputed
 	scanf("%d",&data[0]);
 	//setting the current sum to be the first value of the array
-	current = data[0];
+	current_sum = data[0];
 	
 	//scanning all of the other days and putting them in the array
 	for(int i = 1; i < n; i++) scanf(" %d",&data[i]);
@@ -33,21 +33,21 @@ int main()
 	//for every value of the array
 	for(int i = 1; i < n; i++)
 	{
-		//while the current sum exceeds the max money variable (stated 'K' in the problem), remove the starting elements
-		while(current > k && start < i - 1)
+		//while the current sum exceeds the max money variable (stated 'K' in the problem), remove the starting elements until the current sum is less than the max avaliable money
+		while(current_sum > k && start < i - 1)
 		{
-			current -= data[start];
+			current_sum -= data[start];
 			//increment the starting point
 			start++;
 		}
 		
-		//if the current sum is equal to the max money variable ('k') AND the duration (i-1 - start) is bigger than the biggest duration. The biggest duration is the current duration
-		if(current == k && i-1 - start > out)
+		//if the current sum is equal to the max money variable ('k') AND the duration (i-1 - start) is bigger than the biggest duration. The biggest duration becomes the current duration
+		if(current_sum == k && i-1 - start > out)
 			out = i - start;
 		
 		//if n is grater than i then we add the the i-th element to the current sum
 		if (i < n)
-        	current += data[i]; 
+        	current_sum += data[i]; 
 	}
 	//we print the output and end the program!
 	printf("%d\n",out);
