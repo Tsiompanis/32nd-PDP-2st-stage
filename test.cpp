e82bbbf537137540c27139a06f00a648dbe7ed6b
Peter Tsiompanis <38920641+Tsiompanis@users.noreply.github.com>
2020-03-03 18:29:21 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,58 +1,60 @@
-/* USER: 32pdpu17   
-LANG: C++   
-TASK: longsumk*/    
-#include <stdio.h>          
-#include <cstdio>          
-#include <algorithm>          
-#include <iostream>        
-#include <array>    
-#include <vector>    
-#include <bits/stdc++.h>    
-   
-using namespace std;    
-int data[1000005];  
-  
-int main()  
-{  
-    //initializing freopen functions    
-    freopen("longsumk.in","r",stdin);          
-    freopen("longsumk.out","w",stdout);        
+    /* USER: 32pdpu17   
+    LANG: C++   
+    TASK: longsumk*/    
+    #include <stdio.h>          
+    #include <cstdio>          
+    #include <algorithm>          
+    #include <iostream>        
+    #include <array>    
+    #include <vector>    
+    #include <bits/stdc++.h>    
+       
+    using namespace std;    
+    int data[1000005];  
       
-    int n, k, help, out, current_sum;  
-    int pointer1 = 0;  
-    int pointer2 = 0;  
-      
-    scanf("%d %d",&n, &k);  
-  
-    for(int i = 1; i <= n; i++)  
+    int main()  
     {  
-        scanf("%d",&help);  
-        data[i] = data[i-1] + help;  
-    }  
-      
-    while(pointer2 < n)  
-    {  
-        current_sum = data[pointer2 + 1] - data[pointer1];  
+        //initializing freopen functions    
+        freopen("longsumk.in","r",stdin);          
+        freopen("longsumk.out","w",stdout);        
           
-        if(current_sum == k)  
+        int n, k, help, current_sum;  
+        int out = 0;  
+        int pointer1 = 0;  
+        int pointer2 = 0;  
+          
+        scanf("%d %d",&n, &k);  
+      
+        for(int i = 1; i <= n; i++)  
         {  
-            if(pointer2 - pointer1 + 1 > out)  
-                out = pointer2 - pointer1 + 1;  
+            scanf("%d",&help);  
+            data[i] = data[i-1] + help;  
+        }  
+          
+        while(pointer2 < n)  
+        {  
+            current_sum = data[pointer2 + 1] - data[pointer1];  
               
-            pointer2++;  
-        }  
-          
-        else if(current_sum < k)  
-            pointer2++;  
-          
-        else  
-        {  
-            pointer1++;  
-            if(pointer1 == pointer2)  
+            if(current_sum == k)  
+            {  
+                if(pointer2 - pointer1 + 1 > out)  
+                    out = pointer2 - pointer1 + 1;  
+                  
                 pointer2++;  
-        }  
-  
-    }  
+            }  
+              
+            else if(current_sum < k)  
+                pointer2++;  
+              
+            else  
+            {  
+                pointer1++;  
+                if(pointer1 == pointer2)  
+                    pointer2++;  
+            }  
       
-    printf("%d\n",out);  
-    return 0;  
+        }  
+          
+        printf("%d\n",out);  
+        return 0;  
+    }  
